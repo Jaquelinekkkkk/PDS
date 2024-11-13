3a7913ad5ac186cd437dfcc1203bfde20611f757
Jaquelinekkkkk <165198196+Jaquelinekkkkk@users.noreply.github.com>
2024-11-13 20:27:42 -0300

    PrimeiroFX

---
+++ PrimeiroFX/PrimeiroFX.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package principal;
+
+import java.io.File;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Aluno
+ */
+public class PrimeiroFX extends Application{
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+    URL url = new File("src/principal/sample.fxml").toURI().toURL();
+    Parent  root = FXMLLoader.load(url);
+    Scene cena = new Scene(root);
+    stage.setScene(cena);
+    stage.show();
+    }
+    
+}
